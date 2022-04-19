0dadca4822291923bf38ff901baf73fd256bb7a8
Lane Votapka <lvotapka100@gmail.com>
2022-04-19 13:20:52 -0600

    Fixing file reading bug in long_trajectory.py

--- seekrtools/long_trajectory/long_trajectory.py
+++ seekrtools/long_trajectory/long_trajectory.py
@@ -82,7 +82,7 @@ def anchor_mmvt_output_slicer_dicer(model, anchor):
     for output_file_name in output_file_list:
         with open(output_file_name) as f:
             for line in f.readlines():
-                if line.startswith("#"):
+                if line.startswith("#") or line.startwith("CHECKPOINT"):
                     continue
                 line_split = line.strip().split(",")
                 dest_milestone_alias = int(line_split[0])
